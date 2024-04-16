33f748c462db867a508419b08084f7cb44c72956
Carlos Eduardo Alves de Oliveira <devcarlosedeoliveira@outlook.com>
2024-04-16 12:32:38 -0300

    Mensagens Iniciais

--- ad2_poo/ProjetoAd2POO/src/projetoad2poo/ProjetoAd2POO.java
+++ ad2_poo/ProjetoAd2POO/src/projetoad2poo/ProjetoAd2POO.java
@@ -2,11 +2,22 @@
 package projetoad2poo;
 import java.io.File;
 import java.util.Scanner;
+import java.util.ArrayList;
 public class ProjetoAd2POO {
 
    
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Seja bem-vindo(a)!");
+        System.out.print("Quantas unidades serão cadastradas? ");
+        int totUnidades = sc.nextInt();
+        ArrayList<String> listaCardapio = new ArrayList<String>();
+        for(int i = 1; i <= totUnidades; i++){
+            System.out.print("Arquivo da Unidade " + i + ": ");
+            String unidade = sc.nextLine();
+            listaCardapio.add(unidade);
+            
+        }
     }
     
 }
