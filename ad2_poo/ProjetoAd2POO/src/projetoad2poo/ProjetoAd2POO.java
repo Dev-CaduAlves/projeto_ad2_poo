4b715dcc47791c3745906d235dcb339ad4e46498
Carlos Eduardo Alves de Oliveira <devcarlosedeoliveira@outlook.com>
2024-04-16 23:45:11 -0300

    att

--- ad2_poo/ProjetoAd2POO/src/projetoad2poo/ProjetoAd2POO.java
+++ ad2_poo/ProjetoAd2POO/src/projetoad2poo/ProjetoAd2POO.java
@@ -1,23 +1,33 @@
 
 package projetoad2poo;
-import java.io.File;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 import java.util.ArrayList;
+
 public class ProjetoAd2POO {
 
    
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         Scanner sc = new Scanner(System.in);
         System.out.println("Seja bem-vindo(a)!");
         System.out.print("Quantas unidades serão cadastradas? ");
         int totUnidades = sc.nextInt();
-        ArrayList<String> listaCardapio = new ArrayList<String>();
+        ArrayList<BufferedReader> listaCardapio = new ArrayList<BufferedReader>();
         for(int i = 1; i <= totUnidades; i++){
             System.out.print("Arquivo da Unidade " + i + ": ");
             String unidade = sc.nextLine();
-            listaCardapio.add(unidade);
+            try{
+                FileReader f = new FileReader(unidade);
+                BufferedReader arq = new BufferedReader(f);
+                listaCardapio.add(arq);    
+            }catch(FileNotFoundException e){
+                System.err.println("Erro na abertura do arquivo.");
+            }
             
         }
+        
     }
     
 }
