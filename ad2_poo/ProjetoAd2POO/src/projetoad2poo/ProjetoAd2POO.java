0c489a8413f90977da23a02f77f5bbfbf61e2e26
Carlos Eduardo Alves de Oliveira <devcarlosedeoliveira@outlook.com>
2024-04-26 10:37:09 -0300

    Finalizado

--- ad2_poo/ProjetoAd2POO/src/projetoad2poo/ProjetoAd2POO.java
+++ ad2_poo/ProjetoAd2POO/src/projetoad2poo/ProjetoAd2POO.java
@@ -15,19 +15,18 @@ public class ProjetoAd2POO {
    
     public static void main(String[] args) throws FileNotFoundException, IOException {
         Scanner sc = new Scanner(System.in);
-        ArrayList<Cardapio> cardapios = new ArrayList<Cardapio>(); //Lista que receberá as instâncias da classe Cardápio
+        ArrayList<Cardapio> cardapios = new ArrayList<>(); //Lista que receberá as instâncias da classe Cardápio
         Locale locale = new Locale("pt", "BR");
         NumberFormat formato = NumberFormat.getCurrencyInstance(locale);
-        
+        int totUnidades = 0;
         System.out.println("Seja bem-vindo(a)!");
         System.out.print("Quantas unidades serão cadastradas? ");
-        int totUnidades = sc.nextInt(); //Recebe a entrada de quantidade de Cardápios
+        totUnidades = sc.nextInt(); //Recebe a entrada de quantidade de Cardápios
         sc.nextLine(); //Faz o debuffer do Scanner
-
         for(int i=1; i<=totUnidades; i++){ //Realiza a leitura para cada cardapio fornecido
-            String unidade = "";
+            String unidade = "";                  
             System.out.print("Arquivo da Unidade " + i + ": ");
-            unidade += sc.next();
+            unidade += sc.nextLine();
             String path = System.getProperty("user.dir");
             path += "\\src\\projetoad2poo\\doc\\" + unidade;
             //Bloco que tenta realizar a leitura dos arquivos TXT
@@ -55,12 +54,11 @@ public class ProjetoAd2POO {
                 }
                 cardapios.add(c);
                 }catch(FileNotFoundException e){
-                System.err.println("Erro na abertura do arquivo: " + e.getMessage());
-                System.exit(0);
+                System.err.println("Erro na abertura do arquivo: " + e.getMessage()+"\nTente Novamente!");
+                i--;
             }
         }
         String opcao = "";
-        sc.nextLine();
         while(!"0".equals(opcao)){
             System.out.print("Escolha uma categoria, ou digite “0” para encerrar: ");
             opcao = sc.nextLine().toLowerCase();
